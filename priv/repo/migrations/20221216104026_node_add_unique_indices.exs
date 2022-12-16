c7441db0cfe8c39e7305c8117aacae713e7dfca5
Mikael Brockman <mikael@brockman.se>
2022-12-16 13:08:33 +0200

    wip

--- priv/repo/migrations/20221216104026_node_add_unique_indices.exs
+++
@@ -1,8 +0,0 @@
-defmodule Dumbo.Repo.Migrations.NodeAddUniqueIndices do
-  use Ecto.Migration
-
-  def change do
-    create unique_index(:nodes, :friendly_name)
-    create unique_index(:nodes, :ieee_address)
-  end
-end
